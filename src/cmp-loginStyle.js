ce912433aed457e13121ef692ce4cc638230474a
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-11-08 07:51:27 -1200

    Evidencia Lit-Elemnt

---
+++ src/cmp-loginStyle.js
@@ -0,0 +1,20 @@
+import { css } from "lit-element";
+
+export default css `
+#form{
+    background: #E6E6E6;
+    padding: 98px;
+    border-radius: 20px;
+}
+#form2{
+    background: #E6E6E6;
+    padding: 98px;
+    border-radius: 20px;
+    right: 100px;
+    border:#c72020;
+}
+#container-username{
+    background-color: #da6969;
+}
+
+`
